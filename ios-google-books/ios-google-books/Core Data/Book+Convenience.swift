3112f938e071630c4a1a27f47f5e44f8b30bbdab
Conner Alegre <cga0@pm.me>
2018-08-21 14:45:13 -0600

    implement book extension with convenience initializer

---
+++ ios-google-books/ios-google-books/Core Data/Book+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Book+Convenience.swift
+//  ios-google-books
+//
+//  Created by Conner on 8/21/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Book {
+  convenience init(title: String, author: String, synopsis: String, context: NSManagedObjectContext = CoreDataManager.shared.mainContext) {
+    self.init(context: context)
+    
+    self.title = title
+    self.author = author
+    self.synopsis = synopsis
+  }
+}
--- ios-google-books/ios-google-books/Views/BookTableViewCell.swift
+++ ios-google-books/ios-google-books/Views/BookTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BookTableViewCell: UITableViewCell {
   
+  
   func updateViews() {
     
   }
