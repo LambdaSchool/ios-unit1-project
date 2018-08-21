5ded5cd219980115dc850d2dbf5aefff5c0666d8
Andrew Liao <andrewoliao@gmail.com>
2018-08-21 14:53:23 -0700

    Set up storyboards and implement base ViewControllers with relevant outlets and actions connected

---
+++ Books/Books/View Controllers/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  Books
+//
+//  Created by Andrew Dhan on 8/21/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var readLabel: UILabel!
+    @IBOutlet weak var reviewTextView: UITextView!
+}
---
+++ Books/Books/View/BookshelvesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookshelvesTableViewCell.swift
+//  Books
+//
+//  Created by Andrew Dhan on 8/21/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class BookshelvesTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var isReadLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+}
