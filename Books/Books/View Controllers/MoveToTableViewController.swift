f638d71d42cfec553c94d7d73c582ee90029e049
Linh Bouniol <paulalinh89@gmail.com>
2018-08-23 09:31:59 -0700

    Added sections to moveTVC

--- Books/Books/View Controllers/MoveToTableViewController.swift
+++ Books/Books/View Controllers/MoveToTableViewController.swift
@@ -49,13 +49,15 @@ class MoveToTableViewController: UITableViewController, NSFetchedResultsControll
         
         let fetchRequest: NSFetchRequest<Bookshelf> = Bookshelf.fetchRequest()
         
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "sectionText", ascending: true),
+                                        NSSortDescriptor(key: "identifier", ascending: true),
+                                        NSSortDescriptor(key: "name", ascending: true)]
         
         let moc = CoreDataStack.shared.mainContext
         
         let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
                                              managedObjectContext: moc,
-                                             sectionNameKeyPath: nil,
+                                             sectionNameKeyPath: "sectionText",
                                              cacheName: nil)
         // Set this VC as frc's delegate
         frc.delegate = self
@@ -110,6 +112,14 @@ class MoveToTableViewController: UITableViewController, NSFetchedResultsControll
     
     // MARK: - Table view data source
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return fetchedResultsController.sections?[section].name
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
