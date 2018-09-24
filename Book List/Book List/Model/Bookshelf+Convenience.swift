4f7a3f3af457e4a573e79933841293d47a80f2cc
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-24 12:31:17 -0600

    Layout views and add model object.

---
+++ Book List/Book List/Model/Bookshelf+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Bookshelf+Convenience.swift
+//  Book List
+//
+//  Created by Dillon McElhinney on 9/24/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
