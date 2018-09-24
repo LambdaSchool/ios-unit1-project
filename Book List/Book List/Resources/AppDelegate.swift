a09fc6743f0a53e9ab0d387b7ad7c42f69312a25
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-24 12:05:11 -0600

    Import frameworks for dealing with authorization.

---
+++ Book List/Book List/Resources/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Book List
+//
+//  Created by Dillon McElhinney on 9/24/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GoogleBooksAuthorizationClient.shared.resumeAuthorizationFlow(with: url)
+    }
+
+
+}
+
