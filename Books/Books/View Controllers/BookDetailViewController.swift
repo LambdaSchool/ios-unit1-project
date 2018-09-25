0eec325d9e31cc6e589be48e2a4aa288d27a4215
Daniela Parra <dyparra47@gmail.com>
2018-09-25 10:45:22 -0400

    Set up book detail view controller.

--- Books/Books/View Controllers/BookDetailViewController.swift
+++ Books/Books/View Controllers/BookDetailViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class BookDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private func updateViews() {
+        guard let volume = volume else { return }
+        
+        //book image from string url?
+        bookTitleLabel.text = volume.title
+        bookTitleLabel.text = volume.subtitle
+        authorsLabel.text = volume.authors
+        reviewTextView.text = volume.myReview
+        
+        if volume.hasRead {
+            hasReadButton.setTitle("Have Read", for: .normal)
+        } else {
+            hasReadButton.setTitle("Haven't Read", for: .normal)
+        }
+        
+        //set segmented controller to myRating
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func moveToAnotherBookshelf(_ sender: Any) {
     }
-    */
+    
+    @IBAction func changeHasRead(_ sender: Any) {
+    }
+    
+    // MARK: - Properties
+    
+    var volume: Volume? {
+        didSet {
+            updateViews()
+        }
+    }
+    //add volumecontroller
+    //add bookshelf?
 
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var bookSubtitleLabel: UILabel!
+    @IBOutlet weak var authorsLabel: UILabel!
+    @IBOutlet weak var hasReadButton: UIButton!
+    @IBOutlet weak var reviewSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var reviewTextView: UITextView!
+    
 }
