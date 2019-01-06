a80bf84cdc91edddbc9ac9221867d87d1a3da05d
AustinBCole <austin.cole.chileno@gmail.com>
2019-01-05 17:52:33 -0700

    Finish setting up firebase networking.

--- BookShelf/BookShelf/Models/Firebase.swift
+++ BookShelf/BookShelf/Models/Firebase.swift
@@ -1,20 +1,14 @@
 import Foundation
 
 class Firebase<Item: Codable > {
-    enum Methods {
-        case get
-        case post
-        case put
-        case delete
-    }
-    private var baseURL: URL!  { return URL(string: "https://bookshelf-67481.firebaseio.com/") }
+    static var baseURL: URL!  { return URL(string: "https://put-and-post2.firebaseio.com/") }
     
-    private func requestURL(_ method: Methods, for recordIdentifier: String = "unknownid") -> URL {
+    static func requestURL(_ method: String, for recordIdentifier: String = "unknownid") -> URL {
         switch method {
-        case .get:
+        case "POST":
             // You post to the main DB. It will return a new record identifier
             return baseURL.appendingPathExtension("json")
-        case .post, .put, .delete:
+        case "DELETE", "PUT", "GET":
             // These all work on individual records, and you need to use the
             // record identifier in your URL with one exception, which is when
             // all the records at once, in which case, you do not need the record
@@ -26,30 +20,120 @@ class Firebase<Item: Codable > {
             fatalError("Unknown request method: \(method)")
         }
     }
-    func fetchBookshelves(bookshelves: Bookshelves, completion: @escaping (Error?) -> Void) {
-        let url = requestURL(.get, for: bookshelves.recordIdentifier)
-        URLSession.shared.dataTask(with: url) { (data, _, error)  in
+    
+    // Handle a single request: meant for DELETE, PUT, POST.
+    static func processRequest(
+        method: String, //GET, PUT, POST or DELETE
+        for bookshelves: Bookshelves, // Conforms to Codeable and to Firebase Item
+        with completion: @escaping (_ success: Bool) -> Void = { _ in }
+        ) {
+        
+        // Fetch appropriate request URL customized to method
+        var request = URLRequest(url: requestURL(method, for: bookshelves.recordIdentifier))
+        request.httpMethod = method
+        
+        // Encode this record
+        do {
+            request.httpBody = try JSONEncoder().encode(bookshelves)
+        } catch {
+            NSLog("Unable to encode \(bookshelves): \(error)")
+            completion(false)
+            return
+        }
+        
+        // Create data task to perform request
+        let dataTask = URLSession.shared.dataTask(with: request) { data, _ , error in
+            
+            // Fail on error
             if let error = error {
-                NSLog("Error fetching search results.")
-                completion(error)
+                NSLog("Server \(method) error: \(error)")
+                completion(false)
+                return
+            }
+            
+            // Handle PUT, GET, DELETE and leave
+            guard method == "POST" else {
+                completion(true)
+                return
+            }
+            
+            // Process POST requests
+            
+            // Fetch identifier from POST
+            guard let data = data else {
+                NSLog("Invalid server response data")
+                completion(false)
+                return
+            }
+            
+            do {
+                
+                // POST request returns `["name": recordIdentifier]`. Store the
+                // record identifier
+                let nameDict = try JSONDecoder().decode([String: String].self, from: data)
+                guard let name = nameDict["name"] else {
+                    completion(false)
+                    return
+                }
+                
+                // Update record and store that name. POST is now successful
+                // and includes the recordIdentifier as part of the item record.
+                var bookshelves = bookshelves
+                bookshelves.recordIdentifier = name
+                processRequest(method: "PUT", for: bookshelves)
+                completion(true)
+                
+            } catch {
+                
+                NSLog("Error decoding JSON response: \(error)")
+                completion(false)
+                return
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    static func delete(bookshelves: Bookshelves, completion: @escaping (_ success: Bool) -> Void = { _ in }) {
+        processRequest(method: "DELETE", for: bookshelves, with: completion)
+    }
+    
+    static func save(bookshelves: Bookshelves, completion: @escaping (_ success: Bool) -> Void = { _ in }) {
+        switch bookshelves.recordIdentifier.isEmpty {
+        case true: // POST, new record
+            processRequest(method: "POST", for: bookshelves, with: completion)
+        case false: // PUT, existing record
+            processRequest(method: "PUT", for: bookshelves, with: completion)
+        }
+    }
+    
+    // Fetch all records and pass them to sender via completion handler
+    static func fetchRecords(completion: @escaping ([Item]?) -> Void) {
+        let requestURL = baseURL.appendingPathExtension("json")
+        let dataTask = URLSession.shared.dataTask(with: requestURL) { data, _, error in
+            
+            if let error = error {
+                NSLog("Could not decode data: \(error)")
+                completion(nil)
                 return
             }
             guard let data = data else {
                 NSLog("Could not get data")
-                completion(NSError())
-                return
-            }
-            do {
-              let bookshelves = try JSONDecoder().decode(Bookshelves.self, from: data)
-                Model.shared.bookshelves = bookshelves
                 completion(nil)
                 return
-            } catch {
-                NSLog("Error decoding JSON: \(error)")
-                completion(error)
-                return
             }
-        }.resume()
+            
+            do {
+                let recordDict = try JSONDecoder().decode([String: Bookshelves].self, from:data)
+                let records = recordDict.map({ $0.value })
+                Model.shared.bookshelves = records[0]
+                completion(nil)
+            } catch {
+                NSLog("Error decoding received data: \(error)")
+                completion([])
+            }
+        }
+        
+        dataTask.resume()
     }
-    
 }
